407cec1205a1fd21795ee3ea1b43b6ead80a2901
Yannickkerboull <yannick.kerboull@gmail.com>
2016-07-08 16:09:04 +0200

    1er jet
    Débaras

---
+++ js/script_menu.js
@@ -0,0 +1,58 @@
+/**
+ * Created by Yannick on 07/07/2016.
+ */
+
+$(document).ready(function() {
+    /*
+    $("#voiture_simple_").show();
+    $("#voiture_results_").hide();
+    voiture();
+    */
+    var continued = true;
+
+
+
+    /*
+    Cette méthode est pour si on a 1 points !
+    Hors, il faut le faire sur 2 points pour les imgs
+
+
+    var offset = $( "#tab1" ).offset();
+    var offset1 = $( "#erplan" ).offset();
+    var offset2 = offset.left - offset1.left;
+
+    //Partie touche de l'appli
+    $('#droite').click(function (e) {
+        if (continued) {
+            vjX = parseInt($('#v1').css('left'));
+            if (offset2 < 400)
+                $('#v1').css('left', vjX + 30);
+        }
+    });
+    $('#gauche').click(function (e) {
+        if (continued) {
+            vjX = parseInt($('#v1').css('left'));
+            if (vjX > 70)
+                $('#v1').css('left', vjX - 30);
+        }
+    });
+
+    //Partie touche du clavier
+    $(document).keydown(function (e) {
+        if (continued) {
+            if (e.which == 39) {
+                vjX = parseInt($('#v1').css('left'));
+                if (vjX < 280)
+                    $('#v1').css('left', vjX + 30);
+            }
+            if (e.which == 37) {
+                vjX = parseInt($('#v1').css('left'));
+                if (vjX > 70)
+                    $('#v1').css('left', vjX - 30);
+            }
+        }
+    });
+    */
+
+
+});
